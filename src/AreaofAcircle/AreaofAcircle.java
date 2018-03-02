57e332e9fb2e1c84a939bb4282c4f69bda576917
toluo7449 <toluo7449@7J2M6Y1.Student.UGDSB.ED>
2018-03-02 13:30:22 -0500

    my second commit

---
+++ src/AreaofAcircle/AreaofAcircle.java
@@ -0,0 +1,25 @@
+/*
+ * Sean Luo
+ * areaofacircle.java
+ * This program calculates the AreaOfCircle 
+ */
+
+package AreaofAcircle;
+
+/**
+ *
+ * @author toluo7449
+ */
+public class AreaofAcircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double radius = 15.0;
+       double pi = 3.14;
+       double area = radius*pi*pi;
+    System.out.println("The area is="+area);
+    }
+}
+       //algorithm 
